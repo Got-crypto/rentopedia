e5ad3d429d61d858fa1c48ba8288b40359c8e027
Got-crypto <pecpalmplay@gmail.com>
2023-06-29 01:21:22 +0200

    fixed backend issues for registration

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,31 +1,38 @@
 import prismadb from '@/lib/prismadb'
 import bcrypt from 'bcrypt'
+import { ObjectId } from 'bson'
 
 export default async function register(req, res) {
     if( req.method !== 'POST' ) return res.status(405).json({message: 'wrong method for the URL used. Try to use "GET" insted.'})
 
     try {
         const { email, password: plainText, username } = req.body
-
+        
         const emailIsTaken = await prismadb.users.findUnique({
             where: {
                 email
             }
         })
-
-        if( emailIsTaken ) return res.status(409).json({message: 'Email already in use'})
-
-
+        
+        if( emailIsTaken ) {
+            return res.status(409).json({message: 'Email already in use'})
+        }
+        
+        
         const saltRounds = await bcrypt.genSalt(12)
         const password = await bcrypt.hash(plainText, saltRounds)
+        const id = new ObjectId
 
-        const response = await prismadb?.users?.create({
+        const data = {
             email,
             username,
-            password
-        })
+            password,
+            id: id.toString()
+        }
+        
+        const response = await prismadb?.users?.create({data})
 
-        return res.status(200).json({user: response})
+        return res.status(200).json({user: {username, email, id: response.id, photoUrl: ''}})
     } catch (error) {
         res.status(500).json({message: "server failure", error})
     }
